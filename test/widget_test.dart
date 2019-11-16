0abf76028797bdd4e2cc5a17e75295c7e46d6002
Tobias Olsson <tobias@Tobiass-MacBook-Pro.local>
2019-11-16 18:24:19 +0100

    Update actions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bee_me/main.dart';
+import 'package:My_Bees/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
